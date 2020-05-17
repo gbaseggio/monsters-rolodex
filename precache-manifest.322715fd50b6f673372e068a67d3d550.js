b2134038b680d7d798fca4982939950c9c48205d
Gustavo Baseggio <gbaseggio@gmail.com>
2020-05-17 16:49:17 -0300

    Updates

---
+++ precache-manifest.322715fd50b6f673372e068a67d3d550.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "542775a1544dbe643e51e69f206ec719",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cb1ede673dd53ed70226",
+    "url": "/monsters-rolodex/static/css/main.0be6a56d.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb1ede673dd53ed70226",
+    "url": "/monsters-rolodex/static/js/main.a79b78fb.chunk.js"
+  },
+  {
+    "revision": "e3f9453037e5f28f8167",
+    "url": "/monsters-rolodex/static/js/runtime-main.7c0f1f0f.js"
+  }
+]);
